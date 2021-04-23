da5efab7c8892b11f0fd0605b18ec92dcd8b1b00
cisu <49948070+cisu@users.noreply.github.com>
2021-04-23 14:11:54 +0300

    Adding Form Inputs

---
+++ 4.React_State_and_Working_with_Events/react-complete-guide-state/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+import './ExpenseForm.css'; 
+
+const ExpenseFrom = () => {
+    return(
+        <form action="">
+
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label htmlFor="">Title</label>
+                    <input type="text" name="" id=""/>
+                </div>
+
+                <div className="new-expense__control">
+                    <label htmlFor="">Amount</label>
+                    <input type="number" min='0.01' step='0.01'/>
+                </div>
+
+                <div className="new-expense__control">
+                    <label htmlFor="">Date</label>
+                    <input type="date" min="2019-01-01" max='2022-12-31' id=""/>
+                </div>
+            </div>
+
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+
+        </form>
+    )
+}
+
+export default ExpenseFrom;
