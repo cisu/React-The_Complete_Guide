76045b92f03153440d7a5d59b560dd20167f01ac
george <geor@webdi.com>
2021-07-25 16:15:13 +0300

    Adding a Meals Component

--- 11.food_order_app/src/App.js
+++ 11.food_order_app/src/App.js
@@ -1,10 +1,18 @@
+import {Fragment} from 'react';
+
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 
 function App() {
   return (
     <div>
       <Header />
+      
+      <main>
+        <Meals />
+      </main>
+
     </div>
   );
 }
